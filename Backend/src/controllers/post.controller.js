7a3514ea5f6438962823478ba46f9ce16d8d05e2
Santiago Andres Coraglia Andretich <coragliasantiago@gmail.com>
2025-12-29 08:13:19 +0100

    final commit adding update and delete APIs

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -31,8 +31,58 @@ const getPosts = async (req, res) => {
     try {
 
         const post = await Post.find();
-        res.status(200).json(posts);
+        res.status(200).json(post);
         
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error", error
+        })
+    }
+}
+
+//Update a post
+
+const updatePost = async (req, res) => {
+    try {
+
+        //Validate if the post is empty
+        if(Object.keys(req.body).length === 0) {
+           return res.status(400).json({
+                message: "No data provided for update"
+           });
+        }
+        
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new:true});
+
+        if(!post) return res.status(404).json({
+            message: "Post not found!"
+        });
+
+        res.status(200).json({
+            message: "Post updated succesfully", post
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error", error
+        })
+    }
+}
+
+//Delete Post
+
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id);
+
+        if(!deleted) return res.status(404).json({
+            message: "Post not found!"
+        });
+
+        res.status(200).json({
+            message: "Post successfully deleted!"
+        });
+
     } catch (error) {
         res.status(500).json({
             message: "Internal Server Error", error
@@ -42,5 +92,7 @@ const getPosts = async (req, res) => {
 
 export {
     createPost,
-    getPosts
+    getPosts,
+    updatePost,
+    deletePost
 }; 
